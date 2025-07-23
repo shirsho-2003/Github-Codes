b66253bf447c1b3ae83af22599c524c2447096d0
Shirsho Ghosh <140837367+shirsho-2003@users.noreply.github.com>
2025-07-23 22:44:34 +0530

    Delete fibonacci.py

--- fibonacci.py
+++
@@ -1,8 +0,0 @@
-def fibonacci_series(n):
-    a, b = 0 ,1
-    for _ in range(n):
-        print(a, end=" ")
-        a, b = b, a+b
-
-num = int(input("Enter the length of the numbers: "))
-fibonacci_series(num)
