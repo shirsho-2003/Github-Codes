338bda6d31e1320ec7185c1ec4c4ef590ae7fa98
Shirsho Ghosh <140837367+shirsho-2003@users.noreply.github.com>
2025-07-23 22:44:56 +0530

    Delete swappingnumbers.py

--- swappingnumbers.py
+++
@@ -1,9 +0,0 @@
-a = int(input("enter the first number: "))
-b = int(input("enter the second number: "))
-print("the numbers are: ",a,b)
-temp = a
-a = b
-b = temp
-
-print("after swapping: ")
-print("the numbers are: ",a,b)
