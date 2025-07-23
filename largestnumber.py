30f71fb1e7e76f5a0022a6b1b7c8519d0c14f73f
Shirsho Ghosh <140837367+shirsho-2003@users.noreply.github.com>
2025-07-23 22:44:41 +0530

    Delete largestnumber.py

--- largestnumber.py
+++
@@ -1,7 +0,0 @@
-numbers = input("enter numbers seperated by spaces: ")
-
-numbers_list = [float(num) for num in numbers.split()]
-
-largest = max(numbers_list)
-
-print("the largest number is: ", largest)
