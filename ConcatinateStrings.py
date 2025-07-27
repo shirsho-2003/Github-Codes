d68830a25aab00f09351565d2150543d0c52c617
Shirsho Ghosh <140837367+shirsho-2003@users.noreply.github.com>
2025-07-27 12:56:17 +0530

    Create ConcatinateStrings.py

---
+++ ConcatinateStrings.py
@@ -0,0 +1,8 @@
+def concatinate_strings(str1, str2):
+    return str1 + " " + str2
+
+string1 = input("enter the first string: ")
+string2 = input("enter the second string: ")
+
+concatinate_result = concatinate_strings(string1, string2)
+print("concatinate result is: ", concatinate_result)
