bf0245b36f4cfdcddc7d8f2b60d0d2120b810080
Shirsho Ghosh <140837367+shirsho-2003@users.noreply.github.com>
2025-07-27 14:55:25 +0530

    Create IsPalindrome.py

---
+++ IsPalindrome.py
@@ -0,0 +1,15 @@
+def is_palindrome(string):
+    string = string.replace(" ", "").lower()
+    
+    if len(string) <= 1:
+        return True
+    if string[0] == string[-1]:
+        return is_palindrome(string[1:-1])
+    else:
+        return False
+string = input("enter the string: ")
+
+if is_palindrome(string):
+    print("The string is palindrome.")
+else:
+    print("This is not palindrome.")    
